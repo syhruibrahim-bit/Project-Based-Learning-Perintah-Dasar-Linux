ba8012f32a6d162fd6ce9ffe6308bfa06bfa8d8f
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-10 19:21:06 +0800

    Update BuatStrukturDirektori.sh

--- BuatStrukturDirektori.sh
+++ BuatStrukturDirektori.sh
@@ -10,12 +10,12 @@
 mkdir documents images archives logs
 
 # 2. Membuat file contoh menggunakan echo dan touch.
-#   - for i in {1..5}: melakukan perulangan sebanyak 5 kali.
-#   - echo ... > nama_file : membuat file dan menulis teks ke dalamnya.
-#   - touch : membuat file kosong dengan ekstensi tertentu.
+#  - for i in {1..5}: melakukan perulangan sebanyak 5 kali.
+#  - echo "...." > nama_file : membuat file dan menulis teks ke dalamnya.
+#  - touch : membuat file kosong dengan ekstensi tertentu.
 for i in {1..5}; do
-	echo "ini file documen ke-$i" > documen$i.doc   # Membuat file .doc dengan isi teks
-	echo "ini file arsip ke-$i" > arsip$i.zip       # Membuat file .zip dengan isi teks
-	touch gambar$i.png                              # Membuat file kosong .png
-	touch filelogs$i.log                            # Membuat file kosong .log
+	echo "ini file documen ke-$i" > documen$i.doc
+	echo "ini file arsip ke-$i" > arsip$i.zip 
+	touch gambar$i.png  
+	touch filelogs$i.log 
 done
