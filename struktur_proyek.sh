630f0a771c2aa627877b43fbf4d7ae04c2ee079f
syhruibrahim <syhruibrahim@gmail.com>
2025-11-10 10:03:57 +0800

    Proyek struktur

---
+++ struktur_proyek.sh
@@ -0,0 +1,23 @@
+#=========================================
+#Nama	: Syahru Ramadhan Ibrahim
+#Prodi	: Sistem Informasi/Kelas D
+#Membuat Struktur proyek
+#Project_sistem_operasi_B sesuai latihan
+#========================================
+
+#1. Membuat Direktori Di Home
+cd ~
+mkdir Project_sistem_operasi_B
+cd Project_sistem_operasi_B
+
+#2. Membuat Subdirektori (src,doc,data)
+mkdir src doc data
+
+#3. Membuat file kosong README.sh dan src/main.sh
+touch README.sh src/main.sh
+
+#4. Menampilkan Struktur dan Ukuran proyek dalam format yang mudah di baca
+echo "===Struktur Direktori==="
+ls -R
+echo "===Ukuran Direktori==="
+du -sh ~/Project_sistem_operasi_B
