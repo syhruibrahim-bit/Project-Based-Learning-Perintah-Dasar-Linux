5ea04d0edfcd74fad7011db165845b0723486c66
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-12 14:24:22 +0800

    Update DirectoriProject.sh

--- partbypart/partproject2/DirectoriProject.sh
+++ partbypart/partproject2/DirectoriProject.sh
@@ -1,5 +1,5 @@
 # 1. Berpindah ke direktori home pengguna (~) dan membuat direktori proyek utama
 
 cd ~                                         # Pindah ke direktori home pengguna saat ini
-mkdir Project_sistem_operasi_B               # Membuat folder baru bernama 'Project_sistem_operasi_B'
-cd Project_sistem_operasi_B                  # Masuk ke dalam direktori proyek yang baru dibuat
+mkdir Project_sistem_operasi_D               # Membuat folder baru bernama 'Project_sistem_operasi_B'
+cd Project_sistem_operasi_D                  # Masuk ke dalam direktori proyek yang baru dibuat
