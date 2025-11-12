4ef4f0f60beb45cf2e3d5a959d62b760581ba0f0
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-12 14:23:43 +0800

    Update project_2.sh

--- proyek-2/project_2.sh
+++ proyek-2/project_2.sh
@@ -6,8 +6,8 @@
 
 # 1. Berpindah ke direktori home pengguna (~) dan membuat direktori proyek utama
 cd ~
-mkdir Project_sistem_operasi_B
-cd Project_sistem_operasi_B
+mkdir Project_sistem_operasi_D
+cd Project_sistem_operasi_D
 
 # 2. Membuat subdirektori utama untuk proyek
 mkdir src doc data
@@ -19,4 +19,4 @@ touch README.sh src/main.sh
 echo "===Struktur Direktori==="
 ls -R                                         # Menampilkan semua isi folder dan subfolder
 echo "===Ukuran Direktori==="
-du -sh ~/Project_sistem_operasi_B             # Menampilkan ukuran total direktori proyek
+du -sh ~/Project_sistem_operasi_D             # Menampilkan ukuran total direktori proyek
