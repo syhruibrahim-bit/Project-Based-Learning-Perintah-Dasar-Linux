8767fffc3f56781dd67ac9e3c1042b160f6945a2
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-12 17:54:34 +0800

    Update strukturdirektori.sh

--- partbypart/partproject2/strukturdirektori.sh
+++ partbypart/partproject2/strukturdirektori.sh
@@ -1,5 +1,5 @@
 # 4. Menampilkan struktur direktori dan ukuran total proyek
 echo "===Struktur Direktori==="              # Judul
-ls -R                                        # Menampilkan daftar semua file dan folder secara rekursif (termasuk isi subfolder)
+tree                                         # Menampilkan daftar semua file dan folder secara rekursif (termasuk isi subfolder)
 echo "===Ukuran Direktori==="                # Judul
 du -sh ~/Project_sistem_operasi_D            # Menampilkan total ukuran direktori proyek dalam format yang mudah dibaca (-h = human-readable)
